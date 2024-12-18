b9bfae465b0995c734e82f0fcf300419dfc3f962
Gustavo-Vargas <ing.gustavo.rv@gmail.com>
2024-12-17 18:54:59 -0600

    Agregar url backend render

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 
 // Importamos las rutas para usuarios
 import authRoutes from "./routes/auth.routes.js";
@@ -31,7 +31,12 @@ dotenv.config();
 
 app.use(
 	cors({
-		origin: "http://localhost:5173",
+		origin: [
+			"http://localhost:5173",
+			"http://localhost:4000",
+			"http://localhost",
+			process.env.BASE_URL,
+		],
 		credentials: true,
 	})
 );
